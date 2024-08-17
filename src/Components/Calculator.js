796ded4ff7374f4dd6bb659468a11cf22a3123f9
Udith Wiratunga <udith.wiratunga@gmail.com>
2024-08-17 22:12:42 +0530

    initial Commit

---
+++ src/Components/Calculator.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import './Calculator.css'
+
+const Calculator = () => {
+  const [display,setDisplay] = useState('');
+
+  // function handle click
+  const handleButtonClick =(value) =>{
+    setDisplay(display+value);
+  }
+
+  // function calculate results
+  const calculateResults = () =>{
+    try{
+      setDisplay(eval(display).toString())
+    }
+    catch (error) {
+      setDisplay('Error');
+    }
+  }
+
+  const clearDisplay = () => {
+    setDisplay('');
+  }
+
+
+  return <div className="calculator">
+          <div className='display'>{display}</div>
+          <div>{[
+          "7","8","9","+",
+          "4","5","6","-",
+          "1","2","3","*",
+          "0",".","=","/"
+        ].map((value) => (
+          <button key={value} onClick={()=> value ==='='?calculateResults():handleButtonClick(value)}>{value}</button>
+        ))}</div>
+
+        </div>;
+  
+}
+
+export default Calculator
